48c3bec3c142f432fb2cc72b6b82934871261f94
Jose Pedro Dominguez <78109809+JosePedroDominguez@users.noreply.github.com>
2023-08-14 19:17:55 -0300

    Initial commit

---
+++ PracticaMVC/MVC/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index(string mssg)
+        {
+            ExceptionsView ex = new ExceptionsView
+            {
+                Message = mssg
+            };
+            return View(ex);
+        }
+    }
+
+}
